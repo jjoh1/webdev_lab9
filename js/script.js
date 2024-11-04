63acfade38364dded0fb76cb91931678d072bb21
jjoh1 <jjoh1@cps.edu>
2024-11-03 22:14:21 -0600

    add detailed comments in script.js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ function init(){
   
   function processInput() {
     //get the input
-    var inputBox = document.getElementById('entryinput');
-    var inputText = inputBox.value;
+    var inputBox = document.getElementById('entryinput'); //inputBox stores the HTML element
+    var inputText = inputBox.value; //inputText stores the text value of the box
     
     //set h2
-    var header = document.getElementById('textoutput');
-    header.innerHTML = inputText;
+    var header = document.getElementById('textoutput'); //grab the h2 and store it in "header"
+    header.innerHTML = inputText; //set "header" html to be the text from the input
     
     //alert
-    alert("Justice Oh: " + inputText);
+    alert("Justice Oh: " + inputText); //alert user with text from the input prepended with name
   }
   
   //listen for click
