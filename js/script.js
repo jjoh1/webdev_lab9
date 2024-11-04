5de051f795c507d3db06d4ea97c8619b84898cb7
jjoh1 <jjoh1@cps.edu>
2024-11-03 22:10:54 -0600

    add JavaScript functionality in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  var button = document.getElementById('entrybutton');
+  
+  function processInput() {
+    //get the input
+    var inputBox = document.getElementById('entryinput');
+    var inputText = inputBox.value;
+    
+    //set h2
+    var header = document.getElementById('textoutput');
+    header.innerHTML = inputText;
+    
+    //alert
+    alert("Justice Oh: " + inputText);
+  }
+  
+  //listen for click
+  button.addEventListener('click', processInput);
+}
 
 window.addEventListener('load', init);
